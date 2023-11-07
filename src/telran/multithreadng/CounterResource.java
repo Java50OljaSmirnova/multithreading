c671e3cd8b1f500a31cef7d8bfaccce6dd9e6a98
oljas <olja.smirnov93@gmail.com>
2023-11-07 21:09:43 +0200

    CW#43 solution based non-static mutexes

---
+++ src/telran/multithreadng/CounterResource.java
@@ -0,0 +1,13 @@
+package telran.multithreadng;
+
+public class CounterResource {
+	int counter;
+	public void increment() {
+		counter++;
+	}
+	public int getCounter() {
+		
+		return counter;
+	}
+
+}
