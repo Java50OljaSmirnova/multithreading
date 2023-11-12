9555b5df6fa09d4710180797614f2c06b84e6d63
oljas <olja.smirnov93@gmail.com>
2023-11-12 18:18:02 +0200

    HW#43 with Yura's comments

--- src/telran/multithreadng/game/Race.java
+++ src/telran/multithreadng/game/Race.java
@@ -1,36 +1,41 @@
 package telran.multithreadng.game;
 
+import java.time.Instant;
+import java.util.ArrayList;
+
 public class Race {
 	private int distance;
 	private int minSleep;
 	private int maxSleep;
-	private int winner = -1;
-	public Race(int distance, int minSleep, int maxSleep) {
+	private Instant startTime;
+	private ArrayList<Runner> resultsTable;
+	
+	
+	public Race(int distance, int minSleep, int maxSleep, Instant startTime, ArrayList<Runner> resultsTable) {
 		this.distance = distance;
 		this.minSleep = minSleep;
 		this.maxSleep = maxSleep;
-	}
-	public int getDistance() {
-		
-		return distance;
-	}
-	public int getWinner() {
-		
-		return winner;
-	}
-	public int getMinSleep() {
-		
-		return minSleep;
-	}
-	public int getMaxSleep() {
-		
-		return maxSleep;
-	}
-	public void setWinner(int winner) {
-		
-		if(this.winner == -1) {
-			this.winner = winner;
-		}
+		this.startTime = startTime;
+		this.resultsTable = resultsTable;
 	}
 
+	public Instant getStartTime() {
+		return startTime;
+	}
+
+	public ArrayList<Runner> getResultsTable() {
+		return resultsTable;
+	}
+
+	public int getDistance() {	
+		return distance;
+	}
+	
+	public int getMinSleep() {		
+		return minSleep;
+	}
+	public int getMaxSleep() {		
+		return maxSleep;
+	}
+	
 }
--- src/telran/multithreadng/game/Runner.java
+++ src/telran/multithreadng/game/Runner.java
@@ -1,26 +1,43 @@
 package telran.multithreadng.game;
 
+import java.time.Instant;
+
 public class Runner extends Thread{
 	
 	private Race race;
 	private int runnerId;
+	private Instant finishTime;
 	public Runner(Race race, int runnerId) {
 		this.race = race;
 		this.runnerId = runnerId;
+	}
+	public int getRunnerId() {
+		return runnerId;
+	}
+	public Instant getFinishTime() {
+		return finishTime;
 	}
 	@Override
 	public void run() {
 		int minSleep = race.getMinSleep();
 		int sleepRange = race.getMaxSleep() - minSleep + 1;
-		for(int i = 0; i < race.getDistance(); i++) {
+		int distance = race.getDistance();
+		for(int i = 0; i < distance; i++) {
 			try {
 				sleep((long) (minSleep + Math.random() * sleepRange));
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				throw new IllegalStateException();
 			}
-			System.out.printf("%d - step %d\n", runnerId, i);
+			System.out.println(runnerId);
 		}
-		race.setWinner(runnerId);
+		synchronized (race) {
+			finishTime = Instant.now();
+			finishRace();
+		}
+	}
+	private void finishRace() {
+		race.getResultsTable().add(this);
+		
 	}
 
 }
