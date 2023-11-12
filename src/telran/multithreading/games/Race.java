4145902d45e83074fad69ac15ef9db388b139be2
oljas <olja.smirnov93@gmail.com>
2023-11-12 19:25:31 +0200

    HW#44 atomic

---
+++ src/telran/multithreading/games/Race.java
@@ -0,0 +1,31 @@
+package telran.multithreading.games;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Race {
+	private int distance;
+	private int minSleep;
+	private int maxSleep;
+	AtomicInteger winner = new AtomicInteger(-1);
+	public Race(int distance, int minSleep, int maxSleep) {
+		this.distance = distance;
+		this.minSleep = minSleep;
+		this.maxSleep = maxSleep;
+	}
+	public int getWinner() {
+		return winner.get();
+	}
+	public void setWinner(int winner) {
+		this.winner.compareAndExchange(this.winner.intValue(), winner);
+	}
+	public int getDistance() {
+		return distance;
+	}
+	public int getMinSleep() {
+		return minSleep;
+	}
+	public int getMaxSleep() {
+		return maxSleep;
+	}
+	
+}
